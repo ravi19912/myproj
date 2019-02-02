f3957bb4b5f0573d1b789d549b6186d17a042bd0
ravi19912 <rawithedirector@gmail.com>
2019-02-02 09:40:31 +0000

    new code

---
+++ ravi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main()
+{
+  printf("Hello World\n");
+  return 0;
+}
+#include <stdio.h>
+ 
+int main()
+{
+  int x;
+ 
+  printf("Input an integer\n");
+  scanf("%d", &x); // %d is used for an integer
+ 
+  printf("The integer is: %d\n", x);
+ 
+  return 0;
+}
